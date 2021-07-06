a0cfb9699c0ade149a5b3abb767a1db1b36250af
Mathieu <mathieux51@gmail.com>
2021-07-06 20:17:45 +0200

    Tidy code

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react"
+import App from "./App"
 
-test('renders foo', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/foo/i);
-  expect(linkElement).toBeInTheDocument();
-});
+test("renders foo", () => {
+  render(<App />)
+  const linkElement = screen.getByText(/foo/i)
+  expect(linkElement).toBeInTheDocument()
+})
